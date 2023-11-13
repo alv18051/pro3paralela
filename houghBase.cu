e601e702389f09241e8dec156f4f2b7691ffc064
alv18051 <alv18051@uvg.edu.gt>
2023-11-13 16:51:03 -0600

    fixing some issues but getting the base to fully work

--- houghBase.cu
+++ houghBase.cu
@@ -1,23 +1,126 @@
-/*
- ============================================================================
- Author        : G. Barlas
- Version       : 1.0
- Last modified : December 2014
- License       : Released under the GNU GPL 3.0
- Description   :
- To build use  : make
- ============================================================================
- */
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <cuda.h>
 #include <string.h>
-#include "common/pgm.h"
+#include <assert.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <vector>
+#include <utility>
+
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
+using namespace std;
+
+class PGMImage {
+public:
+    unsigned char* pixels;
+    int x_dim, y_dim, num_colors;
+
+    PGMImage(char* fname) {
+        x_dim = y_dim = num_colors = 0;
+        pixels = NULL;
+
+        FILE* ifile;
+        ifile = fopen(fname, "rb");
+        if (!ifile) return;
+
+        char* buff = NULL;
+        size_t temp;
+
+        fscanf(ifile, "%*s %i %i %i", &x_dim, &y_dim, &num_colors);
+
+        getline(&buff, &temp, ifile);
+
+        assert(x_dim > 1 && y_dim > 1 && num_colors > 1);
+        pixels = new unsigned char[x_dim * y_dim];
+        fread((void*)pixels, 1, x_dim * y_dim, ifile);
+
+        fclose(ifile);
+    }
+
+    PGMImage(int x = 100, int y = 100, int col = 16) {
+        num_colors = (col > 1) ? col : 16;
+        x_dim = (x > 1) ? x : 100;
+        y_dim = (y > 1) ? y : 100;
+        pixels = new unsigned char[x_dim * y_dim];
+        memset(pixels, 0, x_dim * y_dim);
+        assert(pixels);
+    }
+
+    ~PGMImage() {
+        if (pixels != NULL)
+            delete[] pixels;
+        pixels = NULL;
+    }
+
+    bool write(char* fname) {
+        int i, j;
+        FILE* ofile;
+        ofile = fopen(fname, "w+t");
+        if (!ofile) return 0;
+
+        fprintf(ofile, "P5\n%i %i\n%i\n", x_dim, y_dim, num_colors);
+        fwrite(pixels, 1, x_dim * y_dim, ofile);
+        fclose(ofile);
+        return 1;
+    }
+};
+void setPixel(PGMImage &image, int x, int y, unsigned char color) {
+    if (x >= 0 && x < image.x_dim && y >= 0 && y < image.y_dim) {
+        image.pixels[y * image.x_dim + x] = color;
+    }
+}
+
+
+void drawLine(PGMImage &image, int r, float theta, unsigned char color) {
+    int x0, y0, x1, y1;
+    int width = image.x_dim, height = image.y_dim;
+
+    if (theta < M_PI / 4 || theta > 3 * M_PI / 4) { // More horizontal line
+        x0 = 0;
+        y0 = r / sin(theta);
+        x1 = width - 1;
+        y1 = (r - x1 * cos(theta)) / sin(theta);
+    } else { // More vertical line
+        y0 = 0;
+        x0 = r / cos(theta);
+        y1 = height - 1;
+        x1 = (r - y1 * sin(theta)) / cos(theta);
+    }
+
+    // Bresenham's line algorithm to draw the line
+    int dx = abs(x1 - x0), sx = x0 < x1 ? 1 : -1;
+    int dy = -abs(y1 - y0), sy = y0 < y1 ? 1 : -1; 
+    int err = dx + dy, e2;
+
+    int maxIterations = width + height; // A safe upper limit for the number of iterations
+    int iterations = 0;
+
+    while (true) {
+        setPixel(image, x0, y0, color);
+        if (x0 == x1 && y0 == y1 || iterations >= maxIterations) break;
+        e2 = 2 * err;
+        if (e2 >= dy) { err += dy; x0 += sx; }
+        if (e2 <= dx) { err += dx; y0 += sy; }
+        iterations++;
+    }
+
+}
+
+
 
 const int degreeInc = 2;
 const int degreeBins = 180 / degreeInc;
-const int rBins = 100;
+const int rBins = 110;
 const float radInc = degreeInc * M_PI / 180;
 //*****************************************************************
 // The CPU function returns a pointer to the accummulator
@@ -68,48 +171,39 @@ void CPU_HoughTran (unsigned char *pic, int w, int h, int **acc)
 //   //TODO
 // }
 
-// GPU kernel. One thread per image pixel is spawned.
-// The accummulator memory needs to be allocated by the host in global memory
 __global__ void GPU_HoughTran (unsigned char *pic, int w, int h, int *acc, float rMax, float rScale, float *d_Cos, float *d_Sin)
 {
-  //TODO calcular: int gloID = ?
-  int gloID = w * h + 1; //TODO
-  if (gloID > w * h) return;      // in case of extra threads in block
+  // Calculate global ID
+  int gloID = blockIdx.x * blockDim.x + threadIdx.x;
+
+  if (gloID >= w * h) return; // Check if gloID is within the bounds of the image size
 
   int xCent = w / 2;
   int yCent = h / 2;
-
-  //TODO explicar bien bien esta parte. Dibujar un rectangulo a modo de imagen sirve para visualizarlo mejor
   int xCoord = gloID % w - xCent;
   int yCoord = yCent - gloID / w;
 
-  //TODO eventualmente usar memoria compartida para el acumulador
-
   if (pic[gloID] > 0)
+  {
+    for (int tIdx = 0; tIdx < degreeBins; tIdx++)
     {
-      for (int tIdx = 0; tIdx < degreeBins; tIdx++)
-        {
-          //TODO utilizar memoria constante para senos y cosenos
-          //float r = xCoord * cos(tIdx) + yCoord * sin(tIdx); //probar con esto para ver diferencia en tiempo
-          float r = xCoord * d_Cos[tIdx] + yCoord * d_Sin[tIdx];
-          int rIdx = (r + rMax) / rScale;
-          //debemos usar atomic, pero que race condition hay si somos un thread por pixel? explique
-          atomicAdd (acc + (rIdx * degreeBins + tIdx), 1);
-        }
+      float r = xCoord * d_Cos[tIdx] + yCoord * d_Sin[tIdx];
+      int rIdx = (r + rMax) / rScale;
+      atomicAdd (acc + (rIdx * degreeBins + tIdx), 1);
     }
-
-  //TODO eventualmente cuando se tenga memoria compartida, copiar del local al global
-  //utilizar operaciones atomicas para seguridad
-  //faltara sincronizar los hilos del bloque en algunos lados
-
+  }
 }
 
-//*****************************************************************
+
 int main (int argc, char **argv)
 {
+  if (argc < 2) {
+        printf("Usage: %s <input_image.pgm>\n", argv[0]);
+        return -1;
+    }
   int i;
 
-  PGMImage inImg (argv[1]);
+  PGMImage inImg(argv[1]);
 
   int *cpuht;
   int w = inImg.x_dim;
@@ -134,11 +228,11 @@ int main (int argc, char **argv)
     pcSin[i] = sin (rad);
     rad += radInc;
   }
+  printf ("CPU done\n");
 
   float rMax = sqrt (1.0 * w * w + 1.0 * h * h) / 2;
   float rScale = 2 * rMax / rBins;
 
-  // TODO eventualmente volver memoria global
   cudaMemcpy(d_Cos, pcCos, sizeof (float) * degreeBins, cudaMemcpyHostToDevice);
   cudaMemcpy(d_Sin, pcSin, sizeof (float) * degreeBins, cudaMemcpyHostToDevice);
 
@@ -146,9 +240,13 @@ int main (int argc, char **argv)
   unsigned char *d_in, *h_in;
   int *d_hough, *h_hough;
 
-  h_in = inImg.pixels; // h_in contiene los pixeles de la imagen
+  h_in = inImg.pixels;
 
   h_hough = (int *) malloc (degreeBins * rBins * sizeof (int));
+  cudaEvent_t start, stop;
+  cudaEventCreate(&start);
+  cudaEventCreate(&stop);
+  cudaEventRecord(start);
 
   cudaMalloc ((void **) &d_in, sizeof (unsigned char) * w * h);
   cudaMalloc ((void **) &d_hough, sizeof (int) * degreeBins * rBins);
@@ -158,10 +256,44 @@ int main (int argc, char **argv)
   // execution configuration uses a 1-D grid of 1-D blocks, each made of 256 threads
   //1 thread por pixel
   int blockNum = ceil (w * h / 256);
+  
   GPU_HoughTran <<< blockNum, 256 >>> (d_in, w, h, d_hough, rMax, rScale, d_Cos, d_Sin);
 
   // get results from device
   cudaMemcpy (h_hough, d_hough, sizeof (int) * degreeBins * rBins, cudaMemcpyDeviceToHost);
+
+  cudaEventRecord(stop);
+  cudaEventSynchronize(stop);
+  float milliseconds = 0;
+  cudaEventElapsedTime(&milliseconds, start, stop);
+  printf("Kernel execution time: %f milliseconds\n", milliseconds);
+  cudaEventDestroy(start);
+  cudaEventDestroy(stop);
+
+  int threshold = 3500; // Adjust as necessary
+  std::vector<std::pair<int, int>> significant_lines;
+
+  for (int r = 0; r < rBins; r++) {
+        for (int theta = 0; theta < degreeBins; theta++) {
+            if (cpuht[r * degreeBins + theta] > threshold) {
+                significant_lines.push_back(std::make_pair(r, theta));
+            }
+        }
+  }
+  printf("Number of significant lines: %d\n", significant_lines.size());
+
+printf("Drawing lines...\n");
+  unsigned char lineColor = 255; // White color for lines
+  for (auto &line : significant_lines) {
+    int r = line.first;
+    float theta = line.second * radInc; // Convert bin number to angle in radians
+    drawLine(inImg, r, theta, lineColor);
+  }
+  printf("Writing output image...\n");
+
+  //inImg.write("modified_image.pgm");
+  cv::Mat imageMat(inImg.y_dim, inImg.x_dim, CV_8UC1, inImg.pixels);
+  cv::imwrite("output.png", imageMat);
 
   // compare CPU and GPU results
   for (i = 0; i < degreeBins * rBins; i++)
@@ -169,9 +301,18 @@ int main (int argc, char **argv)
     if (cpuht[i] != h_hough[i])
       printf ("Calculation mismatch at : %i %i %i\n", i, cpuht[i], h_hough[i]);
   }
+
   printf("Done!\n");
 
-  // TODO clean-up
+  free(cpuht);
+  free(pcCos);
+  free(pcSin);
+  free(h_hough); 
+
+  cudaFree(d_Cos);
+  cudaFree(d_Sin);
+  cudaFree(d_in);
+  cudaFree(d_hough);
 
   return 0;
 }
--- pgm.cpp
+++ pgm.cpp
@@ -11,15 +11,25 @@
 #include <stdio.h>
 #include <string.h>
 #include <assert.h>
+#include <jpeglib.h>
+#include <math.h>
+#include <vector>
 #include "pgm.h"
 
 using namespace std;
 
 //-------------------------------------------------------------------
-PGMImage::PGMImage(char *fname)
+PGMImage::PGMImage(char *fname, int programID)
 {
-   x_dim=y_dim=num_colors=0;
-   pixels=NULL;
+   if(programID == 0) {
+        this->color = {255,0,0};
+   } else if(programID == 1) {
+        this->color = {0,255,0};
+   } else {
+        this->color = {0,0,255};
+   }
+   this->x_dim=this->y_dim=this->num_colors=0;
+   this->pixels=NULL;
    
    FILE *ifile;
    ifile = fopen(fname, "rb");
@@ -28,43 +38,144 @@ PGMImage::PGMImage(char *fname)
    char *buff = NULL;
    size_t temp;
 
-   fscanf(ifile, "%*s %i %i %i", &x_dim, &y_dim, &num_colors);
+   fscanf(ifile, "%*s %i %i %i", &this->x_dim, &this->y_dim, &this->num_colors);
 
    getline((char **)&buff, &temp, ifile); // eliminate CR-LF
    
-   assert(x_dim >1 && y_dim >1 && num_colors >1);
-   pixels = new unsigned char[x_dim * y_dim];
-   fread((void *) pixels, 1, x_dim*y_dim, ifile);   
+   assert(this->x_dim >1 && this->y_dim >1 && this->num_colors >1);
+   this->pixels = new unsigned char[this->x_dim * this->y_dim];
+   fread((void *) this->pixels, 1, this->x_dim*this->y_dim, ifile);   
    
    fclose(ifile);
 }
 //-------------------------------------------------------------------
 PGMImage::PGMImage(int x=100, int y=100, int col=16)
 {
-   num_colors = (col>1) ? col : 16;
-   x_dim = (x>1) ? x : 100;
-   y_dim = (y>1) ? y : 100;
-   pixels = new unsigned char[x_dim * y_dim];
-   memset(pixels, 0, x_dim * y_dim);
-   assert(pixels);
+   this->num_colors = (col>1) ? col : 16;
+   this->x_dim = (x>1) ? x : 100;
+   this->y_dim = (y>1) ? y : 100;
+   this->pixels = new unsigned char[x_dim * y_dim];
+   memset(this->pixels, 0, this->x_dim * this->y_dim);
+   assert(this->pixels);
 }
 //-------------------------------------------------------------------
 PGMImage::~PGMImage()
 {
-  if(pixels != NULL)
-     delete [] pixels;
-  pixels = NULL;
+  if(this->pixels != NULL)
+     delete [] this->pixels;
+  this->pixels = NULL;
 }
-//-------------------------------------------------------------------
-bool PGMImage::write(char *fname)
-{
-   int i,j;
-   FILE *ofile;
-   ofile = fopen(fname, "w+t");
-   if(!ofile) return 0;
 
-   fprintf(ofile,"P5\n%i %i\n%i\n",x_dim, y_dim, num_colors);
-   fwrite(pixels, 1, x_dim*y_dim, ofile);
-   fclose(ofile);
-   return 1;
+// Helper function to color a pixel in the given buffer
+void colorPixel(unsigned char *colorPixels, int i, unsigned char r, unsigned char g, unsigned char b) {
+    colorPixels[i*3] = r;
+    colorPixels[i*3+1] = g;
+    colorPixels[i*3+2] = b;
+}
+
+// This function writes an image into a JPEG file, coloring certain lines in red
+void PGMImage::write(const char* outputFileName, std::vector<std::pair<int, int>> selectedLines, float radIncrement, int rBins)
+{
+    // Using hypot for better precision and overflow/underflow safety
+    float maxRadius = hypot(this->x_dim, this->y_dim) / 2;
+    float rScale = 2 * maxRadius / rBins;
+    int xCenter = this->x_dim / 2;
+    int yCenter = this->y_dim / 2;
+
+    // Allocate memory for RGB image
+    unsigned char *colorPixels = new unsigned char[this->x_dim * this->y_dim * 3];
+
+    // Store pixel coordinates
+    std::vector<std::pair<int, int>> coordinates(this->x_dim * this->y_dim);
+    for (int i = 0; i < x_dim * y_dim; ++i) {
+        coordinates[i] = std::make_pair(i % this->x_dim - xCenter, yCenter - i / this->x_dim);
+    }
+    for (int i = 0; i < this->x_dim * this->y_dim; ++i) {
+        bool isLine = false;
+        auto [x, y] = coordinates[i];
+
+        for (const auto& line: selectedLines) {
+            int rIdx = line.first;
+            int thetaIdx = line.second;
+            float r = rIdx * rScale - maxRadius;
+            float theta = thetaIdx * radIncrement;
+
+            if (std::abs(r - x * std::cos(theta) - y * std::sin(theta)) < 0.5) {
+                isLine = true;
+                break;
+            }
+        }
+        // Verify if pixel needs to be coloured
+        if (isLine) {
+            colorPixel(colorPixels, i, this->color.at(0), this->color.at(1), this->color.at(2));  // Red color for line pixels
+        } else {
+            colorPixel(colorPixels, i, pixels[i], pixels[i], pixels[i]);  // Grayscale for non-line pixels
+        }
+    }
+
+    /* 
+    Referencia de la documentacion y manejo de la imagen JPEG con la libreria jpeglib.h, https://github.com/kornelski/libjpeg/tree/master
+    poner suma atencion al documento de uso: https://github.com/kornelski/libjpeg/blob/master/usage.doc
+    */
+
+    struct jpeg_compress_struct compressInfo;
+    struct jpeg_error_mgr errorManager;
+    
+    // Open the output file
+    FILE* outputFile = fopen(outputFileName, "wb");
+    if (!outputFile) {
+        printf("Theres an error opening.\n");
+        delete[] colorPixels;
+        return;
+    }
+
+    // Initialize the JPEG compression structures
+    compressInfo.err = jpeg_std_error(&errorManager);
+    jpeg_create_compress(&compressInfo);
+    jpeg_stdio_dest(&compressInfo, outputFile);
+
+    // Set the image properties
+    compressInfo.image_width = this->x_dim;
+    compressInfo.image_height = this->y_dim;
+    compressInfo.input_components = 3; // RGB image
+    compressInfo.in_color_space = JCS_RGB; // RGB color space
+
+    // Set the JPEG compression parameters
+    jpeg_set_defaults(&compressInfo);
+    jpeg_set_quality(&compressInfo, 75, TRUE);
+
+    // Start compression and write the image data
+    jpeg_start_compress(&compressInfo, TRUE);
+    JSAMPROW rowPointer[1];
+    int rowStride = x_dim * 3;
+
+    // Write each row
+    while (compressInfo.next_scanline < compressInfo.image_height) {
+        rowPointer[0] = &colorPixels[compressInfo.next_scanline * rowStride];
+        jpeg_write_scanlines(&compressInfo, rowPointer, 1);
+    }
+
+    // Clean up after compression
+    jpeg_finish_compress(&compressInfo);
+    fclose(outputFile);
+    jpeg_destroy_compress(&compressInfo);
+
+    // Deallocate memory
+    delete[] colorPixels;
+}
+
+int PGMImage::getXDim(void) {
+    return this->x_dim;
+}
+
+int PGMImage::getYDim(void) {
+    return this->y_dim;
+}
+
+int PGMImage::getNumColors(void) {
+    return this->num_colors;
+}
+
+unsigned char* PGMImage::getPixels(void) {
+    return this->pixels;
 }
